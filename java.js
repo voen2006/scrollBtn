0c2c92805f997d9bbf64dacbe1b86ec1b8fed123
voen2006 <65267395+voen2006@users.noreply.github.com>
2023-04-16 01:18:42 +0300

    Add files via upload

--- java.js
+++ java.js
@@ -1,12 +1,26 @@
 const topBtn = document.querySelector('#top-btn');
 document.addEventListener('scroll', function () {
-    if(scrollY > window.innerHeight){
+    if (scrollY > window.innerHeight) {
         topBtn.style.opacity = '1';
         topBtn.style.display = 'block';
 
     }
-     else{
+    else {
         topBtn.style.opacity = '0';
         topBtn.style.display = 'none';
     }
 })
+
+const socioDiv = document.querySelector('.socio-div')
+const socioBtn = document.querySelector('.socio-btn')
+
+let timerWindow = setTimeout(() => {
+    socioDiv.style.transform = 'translateY(100%)';
+}, 1000)
+
+socioBtn.onclick = function () {
+    socioDiv.style.transform = 'translateY(-100%)';
+}
+
+
+
